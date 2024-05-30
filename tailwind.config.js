de1c3cf82d18bfc3baafdc5383a5775dd5974190
Elrey009 <daniloejike@gmail.com>
2024-05-30 15:40:01 +0100

    modified taliwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "#FA8B02",
+        dune: "#333333",
+        lightGray: "#D9D9D9",
+      },
+      backgroundImage: {
+        bgImg1: `url(./assets/julia-solonina.png)`,
+        bgImg2: `url(./assets/about-img.png)`,
+      },
+      fontFamily: {
+        GreatVibes: `"Great Vibes", cursive`,
+        roboto: `"Roboto", sans-serif`,
+      },
+    },
   },
   plugins: [],
 };
